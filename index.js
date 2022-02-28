00cb73c21514569ceb968a570ad4bac9edad24e0
then00bprogrammer <96624909+then00bprogrammer@users.noreply.github.com>
2022-02-28 11:58:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+let n1, n2, res;
+
+function add()
+{
+    n1 = parseFloat(document.getElementById("fn").value);
+    n2 = parseFloat(document.getElementById("sn").value);
+
+    if(n1 && n2)
+    {
+        res = n1 + n2;
+        document.getElementById("ans").value = res;
+    }
+}
+function minusadd()
+{
+    n1 = parseFloat(document.getElementById("fn").value);
+    n2 = parseFloat(document.getElementById("sn").value);
+
+    if(n1 && n2)
+    {
+        res = n1 - n2;
+        document.getElementById("ans").value = res;
+    }
+}
+function mul()
+{
+    n1 = parseFloat(document.getElementById("fn").value);
+    n2 = parseFloat(document.getElementById("sn").value);
+
+    if(n1 && n2)
+    {
+        res = n1 * n2;
+        document.getElementById("ans").value = res;
+    }
+}
+function div()
+{
+    n1 = parseFloat(document.getElementById("fn").value);
+    n2 = parseFloat(document.getElementById("sn").value);
+
+    if(n1 && n2)
+    {
+        res = n1/n2;
+        document.getElementById("ans").value = res;
+    }
+}
