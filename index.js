89d1e4658073360e713fb36b568e039b9a381d43
then00bprogrammer <96624909+then00bprogrammer@users.noreply.github.com>
2022-03-27 17:25:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,8 +45,11 @@ function div()
     }
 }
 function daynknight(){
-    document.body.style.backgroundColor="rgb(246,248,250)";
+    // document.body.style.backgroundColor="rgb(246,248,250)";
+    document.body.style.backgroundColor="silver";
+    document.getElementById('c').style.color="black";
 }
 function knight(){
-    document.body.style.backgroundColor="black";
+    document.body.style.backgroundColor="rgba(1,1,1,0.9)";
+    document.getElementById('c').style.color="white";
 }
